87928851ced6a3830f77c89c733efe068a2b7e16
deepayanbetal1995 <donotreplyairline@gmail.com>
2021-03-05 21:11:27 +0530

    start proj

---
+++ CurrencyFreelanceProj/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurrencyFreelanceProj.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(string uname,string pass)
+        {
+            string username = "max";
+            string password = "max123";
+            if (uname == username && password == pass)
+            {
+                Session["userName"] = username;
+                return RedirectToAction("Index", "CurrencyHome");
+            }
+            else
+                return RedirectToAction("login", "Account");
+
+
+            
+        }
+    }
+}
---
+++ CurrencyFreelanceProj/Controllers/CurrencyHomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurrencyFreelanceProj.Controllers
+{
+    public class CurrencyHomeController : Controller
+    {
+        // GET: CurrencyHome
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
